103044453599da88fef1aaba3bdecd2a509414ae
Raina Masand <rainamasand@gmail.com>
2022-04-25 15:53:01 -0700

    Organize by item

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -31,29 +31,28 @@ class Shop {
         continue;
       }
 
-      if (item.name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        item.decrementQuality();
-      } else {
-        item.incrementQuality();
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 11) {
-            item.incrementQuality();
-          }
-          if (item.sellIn < 6) {
-            item.incrementQuality();
-          }
-        }
-      }
+      item.sellIn = this.items[i].sellIn - 1;
 
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            item.decrementQuality();
-          } else {
-            item.quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
+      if (item.name == 'Aged Brie') {
+        item.incrementQuality();
+        if (item.sellIn < 0) {
           item.incrementQuality();
+        }
+      } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        item.incrementQuality();
+        if (item.sellIn < 11) {
+          item.incrementQuality();
+        }
+        if (item.sellIn < 6) {
+          item.incrementQuality();
+        }
+        if (item.sellIn < 0) {
+          item.quality = 0
+        }
+      } else {
+        item.decrementQuality();
+        if (item.sellIn < 0) {
+          item.decrementQuality();
         }
       }
     }
