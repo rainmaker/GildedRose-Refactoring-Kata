bdd9397bb888db95ceaee1197be70b844e41e508
Raina Masand <rainamasand@gmail.com>
2022-04-25 15:11:27 -0700

    Condense Sulfuras logic

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,11 +12,13 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+        continue;
+      }
+
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+          this.items[i].quality = this.items[i].quality - 1;
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -35,16 +37,13 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+
+      this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+              this.items[i].quality = this.items[i].quality - 1;
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
